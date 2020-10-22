7391d958764d4328cad07466a4185dc45b7c748c
Parv <parvagrw02@gmail.com>
2020-10-22 10:20:01 +0100

    Making some progress

--- matrix.py
+++ matrix.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class ComplexNumber:
-    
+    """A complex number representation"""    
     def __init__(self, real, imaginary):
         self.real = real
         self.imaginary = imaginary
@@ -29,21 +29,22 @@ class ComplexNumber:
             return ComplexNumber(self.real*other, self.imaginary*other)    
 
 class Vector:
-    
+    """Representation of an n-dimensional vector"""
     def __init__(self, dimensions, elements):
         self.dimensions = dimensions
         self.elements = elements
 
     def get_element(self, number):
-        return self.elements[number]=
+        return self.elements[number]
 
 class ComplexVector:
-    
+    """Representation of a complex vector"""
     def __init__(self, dimensions, elements):
         super().__init__()
+    
 
 class Matrix:
-    
+    """Representation of an n-dimensional linear transformation"""
     def __init__(self, dimensions, values):
         """Maps a one dimensional array to a matrix"""
         assert type(dimensions) == tuple
@@ -60,13 +61,25 @@ class Matrix:
     def dot_product(self, other):
         assert isinstance(other, Matrix)
 
+    def eigen(self):
+        """Returns the eigenvector and eigenvalue of the greatest magnitude"""
+        pass
 
     def modulus(self):
+        """Takes the determinant of a matrix"""
         
+        if self.dimensions[0] != self.dimensions[1]:
+            print("Needs to be a square matrice")
+            return 0 
+        
+        if self.dimensions == (2,2):
+            return (self.get_element(0,0)*self.get_element(1,1)) - (self.get_element(1,0)*self.get_element(0,1))
+        else:
+            return 0
         return 
     
 class ComplexMatrix(Matrix):
-    
+    """Representation of a matrix with complex values"""
     def __init__(self, dimensions, values):
         super().__init__(self)
         for i in values:
@@ -75,3 +88,19 @@ class ComplexMatrix(Matrix):
     def get_element(self, r, c):
         return self.elements[r][c]        
 
+class CartesianCoordinate:
+    """Representation of a point"""
+    def __init__(self, x = 0, y = 0 ,z = 0):
+        self.x = x 
+        self.y = y
+        self.z = z
+
+
+def LorentzForce(E, B, v, q):
+    """Takes in the electric field strength, magnetic field strength and scalar
+    charge and returns the lorentz force as a coordinate"""
+    F = CartesianCoordinate()
+    F.x = q*E.x + q*v.x*B.x
+    F.y = q*E.y + q*v.y*B.y
+    F.z = q*E.z + q*v.z*B.z
+    return F
