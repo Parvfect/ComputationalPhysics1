9b723cbb958d45a4c5a45c30817254ca922c971a
Parv Agarwal <65726543+Parvfect@users.noreply.github.com>
2020-11-09 15:54:58 +0000

    Create map.swift

---
+++ CompPhy/Sources/CompPhy/map.swift
@@ -0,0 +1,24 @@
+override func draw(_ rect: NSRect) {
+    NSColor(red: 103/255, green: 146/255, blue: 195/255, alpha: 1).set()
+    let line = NSBezierPath()
+    line.move(to: NSMakePoint(10, 5))
+    line.line(to: NSMakePoint(90, 5))
+    line.lineWidth = 2
+    line.stroke()
+
+    NSColor(red: 163/255, green: 189/255, blue: 218/255, alpha: 1).setFill()
+
+    let origins = [NSPoint(x: 10, y: 1),
+                   NSPoint(x: 50, y: 1),
+                   NSPoint(x: 90, y: 1)]
+
+    let size = NSSize(width: 8, height: 8)
+
+    for origin in origins {
+        let quad = NSBezierPath(roundedRect: NSRect(origin: origin, size: size), xRadius: 2, yRadius: 2)
+        quad.fill()
+        quad.stroke()
+    }
+}
+
+draw()
